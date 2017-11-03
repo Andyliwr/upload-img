6d8a6b30b53aaf3d4c70e662300ddbecdfade9d7
andyliwr <andyliwr@outlook.com>
2017-11-03 23:38:15 +0800

    初步完成上传文件功能

--- api/file.js
+++ api/file.js
@@ -1,204 +1,132 @@
 const convert = require('koa-convert')
 const body = require('koa-better-body')
 const path = require('path')
-const qiniu = require("qiniu");
-const tinify = require("tinify"); // image compress
-const eventproxy = require('eventproxy');
-const uuid = require('uuid');
+const qn = require('qn')
+const tinify = require("tinify") // image compress
+const eventproxy = require('eventproxy')
+const uuid = require('uuid')
 const config = require('../config')
 
 // tinypng上传设置
-tinify.key = "Q0Q_7x6ppzaVY00KSOu1vc5-FVKyN20J";
+tinify.key = "Q0Q_7x6ppzaVY00KSOu1vc5-FVKyN20J"
 
 // qiniu上传设置
-const baseUrl = 'http://oyh0gj8ht.bkt.clouddn.com'
-const qiniu_config = new qiniu.conf.Config()
-/**
- * 华东 qiniu.zone.Zone_z0 upload空间
- * 华南 qiniu.zone.Zone_z2 andyliwr-server空间
- * 具体请参考 https://developer.qiniu.com/kodo/sdk/1289/nodejs
- */
-qiniu_config.zone = qiniu.zone.Zone_z0
-// 是否使用https域名
-qiniu_config.useHttpsDomain = true
-// 上传是否使用cdn加速
-qiniu_config.useCdnDomain = true
-const formUploader = new qiniu.form_up.FormUploader(qiniu_config)
-const putExtra = new qiniu.form_up.PutExtra()
-
-// 压缩
-async function compress(tmpurl) {
-  console.log('压缩', tmpurl)
-  return '压缩后的链接:' + tmpurl
-}
-
-// 上传
-async function upload(tmpurl) {
-  console.log('上传', tmpurl)
-
-}
-
-
-/**
- * @function uptoken 获取上传凭证
- */
-function uptoken() {
-  //需要填写你的 Access Key 和 Secret Key
-  const mac = new qiniu.auth.digest.Mac(config.accessKey, config.secretKey)
-  var options = {
-    scope: config.bucket,
-    returnBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}'
-  }
-  let putPolicy = new qiniu.rs.PutPolicy(options)
-  return putPolicy.uploadToken(mac)
-}
-
-
-/**
- * @function uploadFileToQiNiu 上传文件的调用方法
- * @param localFileUrl 本地图片的url
- * @param remoteFileName 要上传的图片在远程服务器的名字
- * @param success 上传成功的回调
- * @param fail 上传失败的回调
- * @example uploadFileToQiNiu('./reptile/qn_upload/me_test2.jpg', 'me_test2.jpg', success, fail);
- *
- */
-async function uploadFileToQiNiu(localFileUrl, remoteFileName, callback) {
-  console.log(callback)
-  formUploader.putFile(uptoken(), remoteFileName, localFileUrl, putExtra, callback)
-}
+var client = qn.create({
+    accessKey: config.accessKey,
+    secretKey: config.secretKey,
+    bucket: 'upload',
+    origin: 'http://oyh0gj8ht.bkt.clouddn.com',
+});
 
 // image compress
-function imageComparess (url, saveName, callback){
-  var urlReg = new RegExp('[a-zA-z]+://[^\s]*', '')
-  if(urlReg.test(url)){
-    if(typeof callback === "function"){
-      tinify.fromUrl(url).toFile(saveName, callback)
-    }else{
-      tinify.fromUrl(url).toFile(saveName)
+function imageComparess(url, saveName, callback) {
+    var urlReg = new RegExp('[a-zA-z]+://[^\s]*', '')
+    if (urlReg.test(url)) {
+        if (typeof callback === "function") {
+            tinify.fromUrl(url).toFile(saveName, callback)
+        } else {
+            tinify.fromUrl(url).toFile(saveName)
+        }
+    } else {
+        console.log('It is not a url')
+        if (typeof callback === "function") {
+            console.log(url + ' ' + saveName)
+            tinify.fromFile(url).toFile(saveName, callback)
+        } else {
+            tinify.fromFile(url).toFile(saveName)
+        }
     }
-  }else{
-    console.log('It is not a url')
-    if(typeof callback === "function"){
-      console.log(url+' '+saveName)
-      tinify.fromFile(url).toFile(saveName, callback)
-    }else{
-      tinify.fromFile(url).toFile(saveName)
-    }
-  }
 }
 
 // comparess and upload
-function compressAndUpload(url, type, tmpPath, saveName, success, fail){
-  var ep = new eventproxy()
-  ep.all('hasFinishedCompress', function(data){
-    // start to upload
-    console.log('完成压缩'+data)
-    if(typeof success !== 'function' && typeof fail !== 'function'){
-      var success = function (ret) {
-        console.log(url+ '  ===>  ' + 'https://olpkwt43d.qnssl.com/' + ret.key)
-      };
-      var fail = function (err) {
-        console.log(url + '压缩失败....')
-        console.log(err)
-      };
-    }
-    uploadFileToQiNiu(tmpPath +data, 'myapp/'+ (type ? (type + '/') : '') + 'c_' + data, success, fail)
-  });
-  imageComparess(url, tmpPath + saveName, function(){
-    ep.emit('hasFinishedCompress', saveName)
-  })
+function compressAndUpload(url, type, tmpPath, saveName, success, fail) {
+    var ep = new eventproxy()
+    ep.all('hasFinishedCompress', function(data) {
+        // start to upload
+        console.log('完成压缩' + data)
+        if (typeof success !== 'function' && typeof fail !== 'function') {
+            var success = function(ret) {
+                console.log(url + '  ===>  ' + 'https://olpkwt43d.qnssl.com/' + ret.key)
+            };
+            var fail = function(err) {
+                console.log(url + '压缩失败....')
+                console.log(err)
+            };
+        }
+        uploadFileToQiNiu(tmpPath + data, 'myapp/' + (type ? (type + '/') : '') + 'c_' + data, success, fail)
+    });
+    imageComparess(url, tmpPath + saveName, function() {
+        ep.emit('hasFinishedCompress', saveName)
+    })
 }
 
-
-var success = function (res) {
-  console.log('上传成功')
-  console.log(JSON.stringify(res))
-};
-var fail = function (err) {
-  console.log('上传失败')
-  console.log(err)
-};
-// uploadFileToQiNiu(path.join(__dirname + '/../public/images/avatar.jpg'), 'avatar/andyliwr20171103.png')
-// imageComparess('http://avatar.csdn.net/E/2/7/1_u014374031.jpg', './tmp/1.jpg', function(){
-//   console.log('success');
-// })
-// compressAndUpload('http://avatar.csdn.net/E/2/7/1_u014374031.jpg' ,'avatar', './tmp/', 'andyliwr.jpg', success, fail);
-
-export default function (router) {
-  router.post('/api/uploader', convert(body({
-    uploadDir: path.join(__dirname, '/../uploads'),
-    keepExtensions: true
-  })), async (ctx, next) => {
-    // 判断user中的setting是否勾选自动压缩
-    if (ctx.state.user) {
-      // 是否启用图片压缩
-      let isYaSuo = false
-      ctx.state.user.settings.forEach(item => {
-        if ((item.stname === 'compress') && item.value) {
-          isYaSuo = true
-        }
-      })
-      if (isYaSuo) {
-        for (let i = 0; i < ctx.request.files.length; i++) {
-          let compressUrl = await compress(ctx.request.files[i].path)
-          await upload(compressUrl)
-        }
-      } else {
-        for (let i = 0; i < ctx.request.files.length; i++) {
-          await upload(ctx.request.files[i].path)
-        }
-      }
-    } else {
-      for (let i = 0; i < ctx.request.files.length; i++) {
-        let uploadType = ''
-        switch(ctx.request.files[i].type){
-          case 'image/png':
-            uploadType = '.png'
-            break
-          case 'image/jpg':
-            uploadType = '.jpg'
-            break  
-          case 'image/jpeg':
-            uploadType = '.jpg'
-            break
-          case 'image/gif':
-            uploadType = '.gif'
-            break
-          case 'image/bmp':
-            uploadType = '.bmp'
-            break
-          default:
-            break
-        }
-        if(uploadType){
-          await uploadFileToQiNiu(ctx.request.files[i].path, (new Date()).getTime() + uploadType, function(respErr,
-            respBody, respInfo) {
-            if(respErr){
-              ctx.body = {ok:false, msg: '图片上传失败', err: {
-                status: 500,
-                body: respErr
-              }}
+export default function(router) {
+    router.post('/api/uploader', convert(body({
+        uploadDir: path.join(__dirname, '/../uploads'),
+        keepExtensions: true
+    })), async(ctx, next) => {
+        // 判断user中的setting是否勾选自动压缩
+        if (ctx.state.user) {
+            // 是否启用图片压缩
+            let isYaSuo = false
+            ctx.state.user.settings.forEach(item => {
+                if ((item.stname === 'compress') && item.value) {
+                    isYaSuo = true
+                }
+            })
+            if (isYaSuo) {
+                for (let i = 0; i < ctx.request.files.length; i++) {
+                    let compressUrl = await compress(ctx.request.files[i].path)
+                    await upload(compressUrl)
+                }
+            } else {
+                for (let i = 0; i < ctx.request.files.length; i++) {
+                    await upload(ctx.request.files[i].path)
+                }
             }
-            if(respInfo.statusCode == 200){
-              console.log('caonima2')
-              ctx.body = {ok:true, msg: '图片上传成功', data: {
-                url: baseUrl + '/' + respBody.key,
-                fsize: respBody.key,
-                bucket: respBody.bucket
-              }}
-            }else{
-              ctx.body = {ok:false, msg: '图片上传失败', err: {
-                status: respInfo.statusCode,
-                body: respBody
-              }}
+        } else {
+            for (let i = 0; i < ctx.request.files.length; i++) {
+                let uploadType = ''
+                switch (ctx.request.files[i].type) {
+                    case 'image/png':
+                        uploadType = '.png'
+                        break
+                    case 'image/jpg':
+                        uploadType = '.jpg'
+                        break
+                    case 'image/jpeg':
+                        uploadType = '.jpg'
+                        break
+                    case 'image/gif':
+                        uploadType = '.gif'
+                        break
+                    case 'image/bmp':
+                        uploadType = '.bmp'
+                        break
+                    default:
+                        break
+                }
+                if (uploadType) {
+                    return new Promise((resolve, reject) => {
+                        client.uploadFile(ctx.request.files[i].path, { key: (new Date()).getTime() + uploadType }, function(err, result) {
+                            console.log(err, result)
+                            if (err) {
+                                ctx.throw(500, '图片上传失败')
+                                return reject(err)
+                            } else {
+                                ctx.body = {
+                                    ok: true,
+                                    msg: '图片上传成功',
+                                    data: result
+                                }
+                                resolve()
+                            }
+                        })
+                    })
+                } else {
+                    ctx.throw(500, '图片格式不支持')
+                }
             }
-          })
-          console.log('caonima')
-        }else{
-          ctx.body = {ok:false, msg: '图片格式不支持'}
         }
-      }
-    }
-  })
+    })
 }
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,112 @@
 $(document).ready(function() {
-    var main = new uploaderApp.main('upload-area', 'upload-state')
-    main.init()
-})
+    $('#input2').filer({
+        limit: null,
+        maxSize: null,
+        extensions: null,
+        changeInput: '<div class="jFiler-input-dragDrop"><div class="jFiler-input-inner"><div class="jFiler-input-icon"><i class="icon-jfi-cloud-up-o"></i></div><div class="jFiler-input-text"><h3>将文件拖动到此处</h3> <span style="display:inline-block; margin: 15px 0">或者</span></div><a class="jFiler-input-choose-btn blue">选择文件上传</a></div></div>',
+        showThumbs: true,
+        appendTo: null,
+        theme: "dragdropbox",
+        templates: {
+            box: '<ul class="jFiler-item-list"></ul>',
+            item: '<li class="jFiler-item">\
+                            <div class="jFiler-item-container">\
+                                <div class="jFiler-item-inner">\
+                                    <div class="jFiler-item-thumb">\
+                                        <div class="jFiler-item-status"></div>\
+                                        <div class="jFiler-item-info">\
+                                            <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                        </div>\
+                                        {{fi-image}}\
+                                    </div>\
+                                    <div class="jFiler-item-assets jFiler-row">\
+                                        <ul class="list-inline pull-left">\
+                                            <li>{{fi-progressBar}}</li>\
+                                        </ul>\
+                                        <ul class="list-inline pull-right">\
+                                            <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                        </ul>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </li>',
+            itemAppend: '<li class="jFiler-item">\
+                            <div class="jFiler-item-container">\
+                                <div class="jFiler-item-inner">\
+                                    <div class="jFiler-item-thumb">\
+                                        <div class="jFiler-item-status"></div>\
+                                        <div class="jFiler-item-info">\
+                                            <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                        </div>\
+                                        {{fi-image}}\
+                                    </div>\
+                                    <div class="jFiler-item-assets jFiler-row">\
+                                        <ul class="list-inline pull-left">\
+                                            <span class="jFiler-item-others">{{fi-icon}} {{fi-size2}}</span>\
+                                        </ul>\
+                                        <ul class="list-inline pull-right">\
+                                            <li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+                                        </ul>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                        </li>',
+            progressBar: '<div class="bar"></div>',
+            itemAppendToEnd: false,
+            removeConfirmation: false,
+            _selectors: {
+                list: '.jFiler-item-list',
+                item: '.jFiler-item',
+                progressBar: '.bar',
+                remove: '.jFiler-item-trash-action',
+            }
+        },
+        uploadFile: {
+            url: "/api/uploader",
+            data: {},
+            type: 'POST',
+            enctype: 'multipart/form-data',
+            beforeSend: function() {},
+            success: function(data, el) {
+                var parent = el.find(".jFiler-jProgressBar").parent();
+                el.find(".jFiler-jProgressBar").fadeOut("slow", function() {
+                    $("<div class=\"jFiler-item-others text-success\"><i class=\"icon-jfi-check-circle\"></i> Success</div>").hide().appendTo(parent).fadeIn("slow");
+                });
+            },
+            error: function(el) {
+                var parent = el.find(".jFiler-jProgressBar").parent();
+                el.find(".jFiler-jProgressBar").fadeOut("slow", function() {
+                    $("<div class=\"jFiler-item-others text-error\"><i class=\"icon-jfi-minus-circle\"></i> Error</div>").hide().appendTo(parent).fadeIn("slow");
+                });
+            },
+            statusCode: {},
+            onProgress: function() {},
+        },
+        dragDrop: {
+            dragEnter: function() {},
+            dragLeave: function() {},
+            drop: function() {},
+        },
+        addMore: true,
+        clipBoardPaste: true,
+        excludeName: null,
+        beforeShow: function() { return true },
+        onSelect: function() {},
+        afterShow: function() {},
+        onRemove: function() {},
+        onEmpty: function() {},
+        captions: {
+            button: "选择文件",
+            feedback: "选中文件上传",
+            feedback2: "文件已经被选中",
+            drop: "拖放图片或者点击上传",
+            removeConfirmation: "确定要删除此文件吗?",
+            errors: {
+                filesLimit: "Only {{fi-limit}} files are allowed to be uploaded.",
+                filesType: "Only Images are allowed to be uploaded.",
+                filesSize: "{{fi-name}} is too large! Please upload file up to {{fi-maxSize}} MB.",
+                filesSizeAll: "Files you've choosed are too large! Please upload files up to {{fi-maxSize}} MB."
+            }
+        }
+    });
+});
